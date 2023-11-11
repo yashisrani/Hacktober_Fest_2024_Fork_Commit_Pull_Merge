20cf4ec3ede6891e88e2ae1642b1fecd40c183e9
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2023-11-11 20:21:28 +0530

    Create Turbo sort.cpp

---
+++ DSAinC++/Sorting/Turbo sort.cpp
@@ -0,0 +1,21 @@
+// Turbo sort
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int N, A[1000001];
+
+int main()
+{
+    cin >> N;
+    for(int i = 1; i <= N; i++)
+        cin >> A[i];
+  
+    sort(A + 1, A + N + 1);
+  
+    for(int i = 1; i <= N; i++)
+        cout << A[i] << endl;
+        
+    return 0;
+}
